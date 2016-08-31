b95353dca5aeb42bdb8e048a09b378c848a1ca49
Augustin Borsu <a.borsu@gmail.com>
2016-08-31 06:10:14 +0200

    feat(cheeseshop): Package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+  name = 'kgb-rules',
+  packages = ['kgbrules'], # this must be the same as the name above
+  version = '0.0.1',
+  description = 'A commit message linter',
+  author = 'Augustin Borsu',
+  author_email = 'dev@sagacify.com',
+  url = 'https://github.com/Sagacify/kgb-rules'
+)
