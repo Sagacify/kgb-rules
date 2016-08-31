4a3045b65be20010e94f397465122d4213775b55
Augustin Borsu <augustin@sagacify.com>
2016-08-31 10:53:11 +0200

    version(0.0.2): Bump

---
+++ README.md
@@ -0,0 +1,7 @@
+Комитет Git'a Безопасности
+==========================
+
+Комитет Git'a Безопасности (КGБ)  or Git security committee, is a commit message style enforcer.
+
+This repository contains the rules used by both the web-hook and the commit-hook versions of the КGБ.
+For the local hook proceed here, for the webhook proceed [here](https://github.com/Sagacify/komitet-gita-bezopasnosti).
--- setup.py
+++ setup.py
@@ -1,10 +1,27 @@
 from distutils.core import setup
+import io
+
+
+def readme():
+    with io.open('README.md', encoding="utf-8") as f:
+        return f.read()
+
 setup(
-  name = 'kgb-rules',
-  packages = ['kgbrules'], # this must be the same as the name above
-  version = '0.0.1',
-  description = 'A commit message linter',
-  author = 'Augustin Borsu',
-  author_email = 'dev@sagacify.com',
-  url = 'https://github.com/Sagacify/kgb-rules'
+  name='kgb_rules',
+  description='A commit message linter',
+  long_description=readme(),
+  packages=['kgb_rules'],
+  version='0.0.2',
+  author='Augustin Borsu',
+  author_email='dev@sagacify.com',
+  url='https://githubv.com/Sagacify/kgb-rules',
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+    'Natural Language :: English',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.5',
+    'Topic :: Text Processing'
+  ],
+  keywords='git lint russian_history',
 )
